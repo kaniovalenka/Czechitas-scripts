3dbd019e143d6a26f01eeda5aa2a66c4e32f6643
kaniovalenka <lenkakaniova@gmail.com>
2025-10-06 12:50:59 +0200

    change of wording in the last paragraph

--- kaniova_lenka_hw01.py
+++ kaniova_lenka_hw01.py
@@ -13,6 +13,6 @@ for character in text_without_enters.lower():
     else:
         dictionary[character] = 1
 
-with open('hw01_output.json', "w", encoding='utf-8') as file:
+with open('hw01_output.json', mode='w', encoding='utf-8') as file:
         json.dump(dictionary, file, indent=4, sort_keys=True,
                   ensure_ascii=False)    
